a10cc1fcc852fcccf6f7668dad224989eadda31c
antagomir <lei@MIB12001>
2013-02-14 15:49:12 +0100

    R

--- SQL/hitchip-mysql-wur.sh
+++ SQL/hitchip-mysql-wur.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
-mysql -ulmlahti -ppassu Phyloarray < /home/lei/data/HITChip/backup051212.sql
+
+mysql -ulmlahti -ppassu Phyloarray < /home/lei/data/HITChip/phyloarray030213.sql
+# 4.2.2013; 15Gb -> ? hours
+
+#mysql -ulmlahti -ppassu Phyloarray < /home/lei/data/HITChip/backup051212.sql
 # 9.1.2013; 14Gb -> 14 hours
 
 
