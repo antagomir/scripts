6dd4cb0968e6c2a868f2ff959481b5f88b5859c9
antagomir <lei@MIB12001>
2013-03-07 16:41:29 +0100

    hitchips

--- SQL/run.hitchip.R
+++ SQL/run.hitchip.R
@@ -1,8 +1,6 @@
 library(HITChipDB)
 
 # FTP SERVER
-# jsp0812
-# ssh -L 3307:128.214.222.203:3306 wageningen@128.214.222.203
 params <- run.profiling.script(dbuser = "root", dbpwd = "fidipro", dbname = "phyloarray", host = '127.0.0.1', port = 3307)
 
 
