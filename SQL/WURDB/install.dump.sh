a9a79d405f5c8f04f2edc368e464cff4d49b0344
Leo <leo@leo-HP-Compaq-8100-Elite-CMT-PC.(none)>
2013-03-21 10:48:33 +0100

    dump inst

---
+++ SQL/WURDB/install.dump.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# 21.3.2013
+mysql -upit -ppassu Phyloarray_PIT < /home/leo/data/PIT1/DumpPIT1_19032013.sql
+mysql -umit -ppassu Phyloarray_MIT < /home/leo/data/MITChip/DumpMIT_19032013.sql
+# mysql -upit2 -ppassu Phyloarray_PIT2 < /home/leo/data/PIT2/DumpChickenandPIT2_19032013.sql
+# Not sure if mysql went correctly
