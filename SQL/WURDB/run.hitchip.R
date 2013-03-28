548881fb63f12f21aa87c3f66dad9595bc53d621
Leo <leo@leo-HP-Compaq-8100-Elite-CMT-PC.(none)>
2013-03-28 19:18:53 +0100

    OK

--- SQL/WURDB/run.hitchip.R
+++ SQL/WURDB/run.hitchip.R
@@ -1,16 +1,31 @@
-library(HITChipDB)
+# DB installation scripts:
+#~/scripts/SQL/WURDB$ less install.dump.sh
 
-# FTP SERVER
-params <- run.profiling.script(dbuser = "root", dbpwd = "fidipro", dbname = "phyloarray", host = '127.0.0.1', port = 3307)
+# TODO
+# ChickChip
+#library(HITChipDB); params <- run.profiling.script(dbuser = "pit", dbpwd = "passu", dbname = "chickchipdb")
 
 
 # LOCAL
 
-# MITChip OK 14.3.2013
+# MITChip OK 28.3.2013
 library(HITChipDB); params <- run.profiling.script(dbuser = "mit", dbpwd = "passu", dbname = "phyloarray_mit")
 
-# PITChip OK 14.3.2013
+# PITChip OK 28.3.2013
 library(HITChipDB); params <- run.profiling.script(dbuser = "pit", dbpwd = "passu", dbname = "pitchipdb")
+
+# PITChip2 OK 28.3.2013
+library(HITChipDB); params <- run.profiling.script(dbuser = "pit", dbpwd = "passu", dbname = "phyloarray_pit")
+
+# OK 28.3.2013 (phylogeny.full still empty but otherwise OK)
+# FTP SERVER
+# ssh -L 3307:128.214.222.203:3306 wageningen@128.214.222.203 # + salas.
+library(HITChipDB)
+params <- run.profiling.script(dbuser = "root", dbpwd = "fidipro", dbname = "phyloarray", host = '127.0.0.1', port = 3307)
+
+
+
+
 
 
 
