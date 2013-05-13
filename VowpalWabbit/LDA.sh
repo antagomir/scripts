77eea9a49e7ec37b40cee33c9f65ad8824e49103
antagomir <lei@MIB12001>
2013-05-13 14:44:43 +0200

    VW

---
+++ VowpalWabbit/LDA.sh
@@ -0,0 +1,1 @@
+../vowpal_wabbit/vowpalwabbit/vw -d jotain_dataa.dat --lda 20 --passes 40 --cache_file /tmp/vw.cache --lda_alpha 2.5 --lda_rho 0.1 --lda_D 197837 --minibatch 16 --readable_model topic20.dat
