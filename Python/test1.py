11383f35ea5fde8e4741f05fd4dd55524766340b
antagomir <antagomir@opencomp>
2014-01-13 22:41:59 +0100

    py test

---
+++ Python/test1.py
@@ -0,0 +1,11 @@
+#primes = [2,3,5,7]
+for i in xrange(5):
+    print i
+
+# This prints out "Hello, John!"
+name = "John"
+age = 23
+print "Hello, %s!" % name
+print "%s is %d years old" %(name, age)
+
+
