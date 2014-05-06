a36d8813418d94f2d15a34a4b2d2193ac89e93ef
antagomir <antagomir@lei>
2014-05-06 17:14:58 +0200

    ok

--- R/installR.R
+++ R/installR.R
@@ -281,8 +281,9 @@ install.packages("fpc")
 # RStan
 install.packages('inline')
 install.packages('Rcpp')
-options(repos = c(getOption("repos"), rstan = "http://wiki.stan.googlecode.com/git/R"))
+options(repos = c(getOption("repos"), rstan = "http://wiki.rstan-repo.googlecode.com/git/"))
 install.packages('rstan', type = 'source')
+
 
 # Modality tests
 install.packages("diptest")
