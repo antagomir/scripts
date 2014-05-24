b91c2bbfd44086e340e4b1b48259936b08234c83
antagomir <leo.lahti@iki.fi>
2014-05-24 12:32:25 +0200

    legend separation

--- R/RStan/clone.sh
+++ R/RStan/clone.sh
@@ -1,1 +1,2 @@
 git clone https://github.com/euxoa/kilpisjarvi.git
+wget http://www.ling.uni-potsdam.de/~vasishth/JAGSStanTutorial.zip
---
+++ R/ggplot.R
@@ -0,0 +1,11 @@
+# Extract Legend 
+# Usage: grid.draw(g_legend(p2)) 
+g_legend<-function(a.gplot){ 
+  tmp <- ggplot_gtable(ggplot_build(a.gplot)) 
+  leg <- which(sapply(tmp$grobs, function(x) x$name) == "guide-box") 
+  legend <- tmp$grobs[[leg]] 
+  return(legend)
+} 
+
+
+	
