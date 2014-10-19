0f26f784722c53b79a7aebeba5012ec89bfe67a6
antagomir <leo.lahti@iki.fi>
2014-10-19 11:22:28 +0200

    ok

--- R/github/examples.R
+++ R/github/examples.R
@@ -1,1 +1,21 @@
-library(rgithub)
+library(github)
+
+# Get API keys from https://github.com/settings/applications/
+api <- read.csv("api.txt", header = F, row.names = 1)
+client.id <- as.character(api["id",1])
+client.secret <- as.character(api["secret",1])
+
+# Login
+ctx <- interactive.login(client.id, client.secret)
+
+# Get my repo info
+repos <- get.my.repositories(ctx, type="owned", sort="pushed")
+
+# Name of first repo
+repos$content[[1]]$name
+
+# Other stuff
+#me <- get.myself(ctx)
+#me$public_repos
+#star.repository(ctx, "antagomir", "scripts")
+#unstar.repository(ctx, "antagomir", "scripts")
