ba14332f3ce2bd49fa5c5dc4b763b02090ecea12
antagomir <antagomir@lei>
2015-07-30 11:11:06 +0200

    R

--- R/untb.R
+++ R/untb.R
@@ -2,11 +2,20 @@
 # http://finzi.psych.upenn.edu/R/library/untb/html/untb.package.html
 
 library(untb)
-a <- untb(start=rep(1,100),prob=0.005,gens=5000,keep=FALSE)
-preston(a)
-no.of.spp(a)
 
-display.untb(start=rep(1,100),prob=0.1,gens=1000)
+# start: initial community
+# prob: speciation probability
+# gens: generations
+# keep: keep the whole time series
+a <- untb(start=rep(1,100),prob=0.005,gens=5000,keep=TRUE)
 
-data(butterflies)
-plot(butterflies,uncertainty=TRUE)
+
+# Statistics
+#preston(a)
+#no.of.spp(a)
+
+# Animation of community evolution
+# display.untb(start=rep(1,100),prob=0.1,gens=1000)
+
+#data(butterflies)
+#plot(butterflies,uncertainty=TRUE)
