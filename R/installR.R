55dd67ee57d1b8623852b6ec1cabd2fe9036f3ff
antagomir <leo.lahti@iki.fi>
2016-04-02 23:13:15 +0200

    ok

--- R/installR.R
+++ R/installR.R
@@ -391,3 +391,10 @@ install.packages(c("gender", "genderdata"),
                  repos = "http://packages.ropensci.org",
                  type = "source")
 
+# Jupyter
+install.packages(c('rzmq','repr','IRkernel','IRdisplay'),
+                  repos = c('http://irkernel.github.io/',     
+                  getOption('repos')),
+                  type = 'source')
+IRkernel::installspec()
+
