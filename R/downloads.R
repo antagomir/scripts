4b0f84a434554add659616794268904c476170a1
antagomir <leo.lahti@iki.fi>
2018-09-23 18:12:56 +0300

    ok

--- R/downloads.R
+++ R/downloads.R
@@ -4,6 +4,7 @@ library(dplyr)
 
 x <- cran_stats(c("eurostat", "sotkanet", "dmt", "pxweb", "earlywarnings", "fmi"))
 x$year <- format(as.Date(x$start), format="%Y")
+x$month <- format(as.Date(x$start), format="%M")
 
 theme_set(theme_bw(20))
 p1 <- ggplot(x, aes(end, downloads, group=package, color=package)) +
@@ -15,4 +16,14 @@ p2 <- ggplot(x2, aes(year, n, group=package, color=package)) +
 
 library(gridExtra)
 grid.arrange(p1, p2, nrow = 2)
+
+
+df <- x %>% group_by(package) %>% summarise(total = sum(downloads))
+
+df2017 <- x %>% filter(year == 2017) %>% group_by(package, month) %>% summarise(total = sum(downloads), monthly = sum(downloads)/n())
+df2018 <- x %>% filter(year == 2018) %>% group_by(package, month) %>% summarise(total = sum(downloads), monthly = sum(downloads)/n())
+
+
+library(knitr)
+kable(df)
 
