93e76d0961d181af29a814b78b8d7913bdff9c53
antagomir <leo.lahti@iki.fi>
2023-01-27 19:47:25 +0200

    ok

---
+++ R/install_universe.R
@@ -0,0 +1,8 @@
+# Enable the r-universe repo
+options(repos = c(
+    fawda123 = 'https://fawda123.r-universe.dev',
+    CRAN = 'https://cloud.r-project.org'))
+
+# Install ggord
+install.packages('ggord')
+
